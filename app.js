68f33d31eba31cc606335e919f52857e95a9492f
Fabio Albertin <albertin@gmail.com>
2017-04-09 22:35:14 +0100

    Finished level 3

--- app.js
+++ app.js
@@ -7,9 +7,49 @@ app.use(logger);
 
 app.use(express.static('public'));
 
-app.get('/blocks', function(req,res){
-  var blocks = ['Fixed', 'Movable','Rotating'];
-  res.json(blocks);
+var blocks = {
+  'Fixed': 'Fastened securely in position',
+  'Movable': 'Capable of being moved',
+  'Rotating': 'Moving in a circle around its center'
+};
+
+var locations = {
+  'Fixed': 'First floor',
+  'Movable': 'Second floor',
+  'Rotating': 'Penthouse'
+};
+
+app.param('name', function(req, res, next) {
+  var name = req.params.name;
+  var block = name[0].toUpperCase() + name.slice(1).toLowerCase();
+
+  req.blockName = block;
+
+  next();
+});
+
+app.get('/blocks', function(req, res) {
+  res.json(Object.keys(blocks));
+});
+
+app.get('/blocks/:name', function(req, res){
+  var description = blocks[req.blockName];
+
+  if (!description) {
+    res.status(404).json('No description found for ' + req.params.name);
+  } else {
+    res.json(description);
+  }
+});
+
+app.get('/locations/:name', function(req, res){
+  var location = locations[req.blockName];
+
+  if (!location) {
+    res.status(404).json('No description found for ' + req.params.name);
+  } else {
+    res.json(location);
+  }
 });
 
 app.get('/parts', function(req,res){
